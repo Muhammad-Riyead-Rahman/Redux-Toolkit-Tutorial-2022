4ba8f7c4e5ca3d59bea2151045371c081003307f
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-07-02 00:40:22 +0600

    react redux counter app

---
+++ my-app/src/services/reducers/counterReducer.js
@@ -0,0 +1,36 @@
+import { DECREMENT, INCREMENT, RESET } from "../constants/counterConstant";
+
+const initialState = {
+  count: 0
+}
+
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1
+      }
+      break;
+    
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+      break;
+    
+    case RESET:
+      return {
+        ...state,
+        count: 0
+      }
+      break;
+  
+    default:
+      return state;
+  }
+}
+
+export default counterReducer;
