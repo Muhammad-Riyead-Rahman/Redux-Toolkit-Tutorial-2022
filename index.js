317b40a752d007c2afdda2300e5eade3a801b1f5
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-07-01 11:47:15 +0600

    creating reducer function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //Defining constant
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
@@ -22,3 +23,28 @@ const decrementCounterAction = () => {
     type: DECREMENT
   }
 }
+
+//Create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1
+      }
+      
+      break;
+    
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+      break;
+  
+    default:
+      state;
+  }
+}
+
+
