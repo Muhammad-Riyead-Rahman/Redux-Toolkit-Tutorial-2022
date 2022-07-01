7c5d42f862023a2e7a470ef594f267b115546a76
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-07-01 19:40:04 +0600

    com.

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const {createStore} = require('redux');
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const RESET = "RESET";
+const INCREMENT_COUNTER_BY_PAYLOAD = "INCREMENT_COUNTER_BY_PAYLOAD";
+const INCREMENT_USER = "INCREMENT_USER";
 
 //Defining state
 const initialCounterState = {
   count: 0,
+  users: ["Riyead"]
 };
 
 //Create action (Action are object type, that are two things - type, payload)
@@ -34,6 +37,22 @@ const resetCounterAction = () => {
   }
 } 
 
+//Increment counter by payload
+const incrementCounterByPaloadAction = (value) => {
+  return {
+    type: INCREMENT_COUNTER_BY_PAYLOAD,
+    payload: value
+  }
+}
+
+//Increment user
+const incrementUserAction = (user) => {
+  return {
+    type: INCREMENT_USER,
+    payload: user
+  }
+}
+
 //Create reducer for counter
 const counterReducer = (state = initialCounterState, action) => {
   switch (action.type) {
@@ -50,6 +69,7 @@ const counterReducer = (state = initialCounterState, action) => {
         ...state,
         count: state.count - 1
       }
+    
       break;
     
     case RESET:
@@ -57,6 +77,23 @@ const counterReducer = (state = initialCounterState, action) => {
         ...state,
         count: 0
       }
+    
+      break;
+    
+    case INCREMENT_COUNTER_BY_PAYLOAD:
+      return {
+        ...state,
+        count: state.count + action.payload
+      }
+    
+      break;
+    
+    case INCREMENT_USER:
+      return {
+        ...state,
+        users: [...state.users, action.payload]
+      }
+      break;
   
     default:
       state;
@@ -77,3 +114,5 @@ store.dispatch(incrementCounterAction());
 store.dispatch(decrementCounterAction());
 store.dispatch(decrementCounterAction());
 store.dispatch(resetCounterAction());
+store.dispatch(incrementCounterByPaloadAction(5));
+store.dispatch(incrementUserAction(" Harun"));
