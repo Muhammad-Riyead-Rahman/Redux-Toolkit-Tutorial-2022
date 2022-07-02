10cde55d4cf2cb1fc33dbf5fe77249b6564af859
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-07-02 10:27:50 +0600

    data fatching using react-redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Counter from "./components/Counter";
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to react-redux</h1>
-      <Counter />
+      <Todos />
     </div>
   );
 }
